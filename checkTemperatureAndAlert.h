93de12721bd774cdbc91eab463886d922e494654
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-07-19 21:19:55 +0530

    Update and rename typewise-alert.h to checkTemperatureAndAlert.h

---
+++ checkTemperatureAndAlert.h
@@ -0,0 +1,43 @@
+
+
+typedef enum {
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+} CoolingType;
+
+typedef enum {
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+
+typedef enum {
+  TO_CONTROLLER,
+  TO_EMAIL
+} AlertTarget;
+
+typedef struct {
+  CoolingType coolingType;
+  char brand[48];
+} BatteryCharacter;
+
+typedef struct{
+	int upperLimit;
+	int lowerLimit;
+}TemperatureLimits;
+
+typedef AlertTarget (*alertFunction) (BreachType breachType);
+
+AlertTarget sendToController(BreachType breachType);
+AlertTarget sendToEmail(BreachType breachType);
+BreachType CheckUpperLimitBreach(double value, double upperLimit);
+BreachType CheckLowerLimitBreach(double value, double lowerLimit);
+TemperatureLimits SetPassiveCoolingLimit(CoolingType coolingTyp,TemperatureLimits passiveCoolingLimit);
+TemperatureLimits SetHiActiveCoolingLimit(CoolingType coolingTyp,TemperatureLimits HiActiveCoolingLimit);
+TemperatureLimits SetMedActiveCoolingLimit(CoolingType coolingTyp,TemperatureLimits MedActiveCoolingLimit);
+AlertTarget checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+AlertTarget print_AlertInfo(AlertTarget alertTarget,BreachType breachType,alertFunction *SendAlert);
