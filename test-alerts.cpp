f4efdfd515a7ba6f1f4143444abb81f0f0ec4d3c
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-07-19 22:07:17 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,24 @@
 #include "test/catch.hpp"
 #include "checkTemperatureAndAlert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+TEST_CASE("Check the breach type for upper limit") 
+{
+  REQUIRE(CheckUpperLimitBreach(12, 30) == NORMAL);
+  REQUIRE(CheckUpperLimitBreach(31, 30) == TOO_HIGH);
+  REQUIRE(CheckUpperLimitBreach(30, 30) == NORMAL);  
+}
+
+TEST_CASE("Check the breach type for lower limit") 
+{
+  REQUIRE(CheckLowerLimitBreach(12, 20) == TOO_LOW);
+  REQUIRE(CheckLowerLimitBreach(31, 20) == NORMAL);
+  REQUIRE(CheckLowerLimitBreach(20, 20) == NORMAL);  
+}
+
+TEST_CASE("Check the limits of passive cooling") 
+{
+  TemperatureLimits passiveLimit={25,35};
+  TemperatureLimits passiveLimit_expected={35,0};
+  REQUIRE(SetPassiveCoolingLimit(PASSIVE_COOLING,passiveLimit) == passiveLimit_expected);
+  REQUIRE(SetPassiveCoolingLimit(HI_ACTIVE_COOLING,passiveLimit) == passiveLimit); 
 }
