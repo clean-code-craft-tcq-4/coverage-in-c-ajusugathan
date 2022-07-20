03506b7118a66e9fbe3d1bd89ba88097c8b862e4
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-07-20 13:43:52 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,21 +4,21 @@
 #include "string.h"
 #include "checkTemperatureAndAlert.h"
 
-TEST_CASE("Check the breach type for upper limit") 
+TEST_CASE("Checks the breach type for upper limit") 
 {
   REQUIRE(CheckUpperLimitBreach(12, 30) == NORMAL);
   REQUIRE(CheckUpperLimitBreach(31, 30) == TOO_HIGH);
   REQUIRE(CheckUpperLimitBreach(30, 30) == NORMAL);  
 }
 
-TEST_CASE("Check the breach type for lower limit") 
+TEST_CASE("Checks the breach type for lower limit") 
 {
   REQUIRE(CheckLowerLimitBreach(12, 20) == TOO_LOW);
   REQUIRE(CheckLowerLimitBreach(31, 20) == NORMAL);
   REQUIRE(CheckLowerLimitBreach(20, 20) == NORMAL);  
 }
 
-TEST_CASE("Check the limits of passive cooling") 
+TEST_CASE("Checks the limits of passive cooling") 
 {
   TemperatureLimits passiveLimit={25,35};
   TemperatureLimits passiveLimit_expected={35,0};
@@ -34,7 +34,7 @@ TEST_CASE("Check the limits of passive cooling")
   
 }
 
-TEST_CASE("Check the limits of High Active cooling") 
+TEST_CASE("Checks the limits of High Active cooling") 
 {
   TemperatureLimits hiActiveLimit={25,35};
   TemperatureLimits hiActiveLimit_expected={45,0};
@@ -49,7 +49,7 @@ TEST_CASE("Check the limits of High Active cooling")
   REQUIRE(hiActiveLimit_return.lowerLimit == hiActiveLimit.lowerLimit); 
 }
 
-TEST_CASE("Check the limits of Medium Active cooling") 
+TEST_CASE("Checks the limits of Medium Active cooling") 
 {
   TemperatureLimits medActiveLimit={25,35};
   TemperatureLimits medActiveLimit_expected={40,0};
@@ -73,7 +73,7 @@ TEST_CASE("infers the breach according to limits")
   REQUIRE(inferBreach(25, 10, 25) == NORMAL);
 }
 
-TEST_CASE("check the classification of temperature breach") 
+TEST_CASE("checks the classification of temperature breach") 
 {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 45) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 35) == NORMAL);
@@ -95,7 +95,7 @@ TEST_CASE("check the classification of temperature breach")
 
 }
 
-TEST_CASE("check the alert type for the breach") 
+TEST_CASE("checks for the alert type of the breach") 
 {
   BatteryCharacter battery_one = {PASSIVE_COOLING,"info"};
   REQUIRE(checkAndAlert(TO_CONTROLLER, battery_one, 40) == TO_CONTROLLER);
@@ -110,7 +110,7 @@ TEST_CASE("check the alert type for the breach")
   REQUIRE(checkAndAlert(TO_EMAIL, battery_three, 45) == TO_EMAIL);
 }
 
-TEST_CASE("check the alert type and printing info") 
+TEST_CASE("checks for the alert type and printing info") 
 {
   alertFunction fpSendAlert_test[2] = {sendToController,sendToEmail};
   char msg[50]={"feed : 0\n"};
@@ -136,4 +136,34 @@ TEST_CASE("check the alert type and printing info")
   char EmailMsg_two[50]={"To: a.b@c.com\nHi, the temperature is TOO_HIGH\n"};
   REQUIRE(print_AlertInfo(TO_EMAIL,TOO_HIGH,fpSendAlert_test) == TO_EMAIL);
   REQUIRE(strcmp(EmailMsg_two,Alert_Info)==0);
+}
+
+TEST_CASE("checks for Controller alert alert info and function return")
+{
+  char *msg_controller={"feed : 0\n"};
+  REQUIRE(sendToController(NORMAL) == TO_CONTROLLER);
+  REQUIRE(strcmp(msg_controller,Alert_Info)==0);
+  
+  char *msg_controller_one={"feed : 1\n"};
+  REQUIRE(sendToController(TOO_LOW) == TO_CONTROLLER);
+  REQUIRE(strcmp(msg_controller_one,Alert_Info)==0);
+  
+  char *msg_controller_two={"feed : 2\n"};
+  REQUIRE(sendToController(TOO_HIGH) == TO_CONTROLLER);
+  REQUIRE(strcmp(msg_controller_two,Alert_Info)==0);
+}
+
+TEST_CASE("checks for Email alert alert info and function return")
+{
+  char *msg_email={"To: a.b@c.com\nHi, the temperature is NORMAL\n"};
+  REQUIRE(sendToController(NORMAL) == TO_EMAIL);
+  REQUIRE(strcmp(msg_email,Alert_Info)==0);
+  
+  char *msg_email_one={"To: a.b@c.com\nHi, the temperature is TOO_LOW\n"};
+  REQUIRE(sendToController(TOO_LOW) == TO_EMAIL);
+  REQUIRE(strcmp(msg_email_one,Alert_Info)==0);
+  
+  char *msg_email_two={"To: a.b@c.com\nHi, the temperature is TOO_HIGH\n"};
+  REQUIRE(sendToController(TOO_HIGH) == TO_EMAIL);
+  REQUIRE(strcmp(msg_email_two,Alert_Info)==0);
 }
