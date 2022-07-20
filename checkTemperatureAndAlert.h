e030f1b132a44c47b48ac5aa16065891c8c29630
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-07-20 12:47:34 +0530

    Update checkTemperatureAndAlert.h

--- checkTemperatureAndAlert.h
+++ checkTemperatureAndAlert.h
@@ -3,7 +3,8 @@
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
+  MED_ACTIVE_COOLING,
+  DEFAULT_STATE   //this state added only for testing
 } CoolingType;
 
 typedef enum {
